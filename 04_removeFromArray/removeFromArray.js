7fb592235b3c7743fb695d928e5c99b1e3a5156c
Aonkyanu C. Tomal <ansatsuken1@outlook.com>
2024-09-29 00:48:13 +0600

    Modify 04_removeFromArray as part of an exercise Modified 04_removeFromArray as directed by the foundations curriculum of the Odin Project for exercise.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    let filtered = array.filter(x => !args.includes(x));
+
+    return filtered;
 };
+
+removeFromArray([1,2,3,4], 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
