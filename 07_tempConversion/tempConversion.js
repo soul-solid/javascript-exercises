72027d1ac754ad9039933aa18fdcc2404ea84a37
Aonkyanu C. Tomal <ansatsuken1@outlook.com>
2024-09-30 04:08:07 +0600

    Modify 07_tempConversion of TOP javascript exercises Modified 07_tempConversion javascript exercise as part of the Odin Project's (TOP) foundations curriculum.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num1) {
+  let fh = (num1 - 32) * 5 / 9;
+
+  let roundedFh = Math.round(fh * 10) / 10;
+
+  return roundedFh;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num2) {
+  let cs = (num2 * 1.8) + 32;
+
+  let roundedCs = Math.round(cs * 10) / 10;
+
+  return roundedCs;
 };
 
 // Do not edit below this line
