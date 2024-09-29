07f3cff3b3ffbbb208e660a033b8aa7deadfe219
Aonkyanu C. Tomal <ansatsuken1@outlook.com>
2024-09-30 01:13:50 +0600

    Modify 05_sumAll of javascript exercises Modified 05_sumAll javascript exercise as per the directions of the Odin Project for exercise.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,37 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {    
+    let sum = 0;
+    
+    const high = Math.max(num1, num2);
+    const low = Math.min(num1, num2);
 
+    if (high < 0 || low < 0) {
+        return "ERROR";
+    } else if (isNonNumber(num1) === true || isNonNumber(num2) === true) {
+        return "ERROR";
+    } else if (isString(num1) === true || isString(num2) === true) {
+        return "ERROR";
+    } else {
+        sum = (high * (high + 1) / 2) - ((low - 1) * low / 2);
+    }
+
+    return sum;
 };
+
+const isNonNumber = function(n) {
+    if (n % 1 !== 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+const isString = function(n) {
+    if (typeof n === 'string') {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 // Do not edit below this line
 module.exports = sumAll;
