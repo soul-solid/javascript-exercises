076e5e0fc031a645721f51cfaebd072601ddfc6b
Aonkyanu C. Tomal <ansatsuken1@outlook.com>
2024-10-19 00:40:00 +0600

    Modify 08_calculator.js Modified 08_calculator.js of javascript-exercises as directed by the odin project's curriculum.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return (a) - (b);
 };
 
-const sum = function() {
-	
+const sum = function([...sumArray]) {
+	return sumArray.reduce((total, num) => total + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function([...multiArray]) {
+  return multiArray.reduce((total, num) => total * num, 1)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0 || num === 1) {
+    return 1;
+  }
+
+  for (let i = num - 1; i >= 1; i--) {
+    num *= i;
+  }
+
+  return num;
 };
 
 // Do not edit below this line
