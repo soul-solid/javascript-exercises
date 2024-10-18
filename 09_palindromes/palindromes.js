3bee95026d4ffff0e1ca1c63522a59346bde6631
Aonkyanu C. Tomal <ansatsuken1@outlook.com>
2024-10-19 01:25:18 +0600

    Modify 09_palindromes.js Modified 09_palindromes.js file of javascript-exercises as directed by the odin project's curriculum.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let cleanedStr = string.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+    let reversedStr = cleanedStr.split("").reverse().join("");
 
+    if (reversedStr === cleanedStr) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
