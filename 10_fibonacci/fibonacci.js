e76c37e3fc57e4ec00f7edd236806cc0c1c1d48a
Aonkyanu C. Tomal <ansatsuken1@outlook.com>
2024-10-19 21:23:32 +0600

    Modify 10_fibonacci.js of javascript-exercises Modified the 10_fibonacci.js file according to the direction of the odin project's curriculum.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (+num === 0) return 0;
+    if (num === 1) return 1;
+    if (num < 0) return "OOPS";
 
+    let prev = 0;
+    let current = 1;
+
+    for (let i = 2; i <= num; i++) {
+        let next = prev + current;
+        prev = current;
+        current = next;
+    }
+
+    return current;
 };
 
 // Do not edit below this line
