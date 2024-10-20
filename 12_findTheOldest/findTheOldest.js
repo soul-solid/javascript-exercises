c865ce4c7c0f81fca263be4334ebce65087e0e05
Aonkyanu C. Tomal <ansatsuken1@outlook.com>
2024-10-20 16:01:29 +0600

    Modify 12_findTheOldest.js of javascript-exercises Modified 12_findTheOldest.js file of javascript-exercises as directed by the odin project's curriculum.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    let currentYear = new Date().getFullYear();
 
+    array.sort((a, b) => {
+        let presentA = a.yearOfDeath || currentYear;
+        let presentB = b.yearOfDeath || currentYear;
+        return (presentB - b.yearOfBirth) - (presentA - a.yearOfBirth);
+    });    
+    return array[0];
 };
 
 // Do not edit below this line
